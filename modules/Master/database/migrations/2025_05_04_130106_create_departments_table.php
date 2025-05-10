cebbb2f4a481f54175f4c0ad713fdaabbef9aefa
KhenCahyo13 <khencahyo02@gmail.com>
2025-05-10 13:52:51 +0700

    feat: Master data migration

--- modules/Master/database/migrations/2025_05_04_130106_create_departments_table.php
+++ modules/Master/database/migrations/2025_05_04_130106_create_departments_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('departments', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->string('name', 50);
+            $table->string('name', 150);
             $table->timestamps();
         });
     }
--- modules/Master/database/migrations/2025_05_04_130115_create_study_programs_table.php
+++ modules/Master/database/migrations/2025_05_04_130115_create_study_programs_table.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Modules\Master\Models\Department;
+use Modules\Master\Models\StudyProgram;
 
 return new class extends Migration
 {
@@ -14,9 +16,65 @@ return new class extends Migration
         Schema::create('study_programs', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->foreignUuid('department_id')->references('id')->on('departments')->onDelete('cascade')->onUpdate('cascade');
-            $table->string('name', 50);
+            $table->string('name', 150);
             $table->timestamps();
         });
+
+        $departments = [
+            'Information Technology' => [
+                'D2 Software Development',
+                'D4 Informatics Engineering',
+                'D4 Business Information Systems',
+            ],
+            'Mechanical Engineering' => [
+                'D3 Mechanical Engineering',
+                'D4 Automotive Electronics Engineering',
+                'D4 Production and Maintenance Mechanical Engineering',
+            ],
+            'Accounting' => [
+                'D3 Accounting',
+                'D4 Finance',
+                'D4 Management Accounting'
+            ],
+            'Chemical Engineering' => [
+                'D3 Chemical Engineering',
+                'D4 Chemical Engineering'
+            ],
+            'Business Administration' => [
+                'D3 Business Administration',
+                'D3 English',
+                'D4 Marketing Management',
+                'D4 English'
+            ],
+            'Electrical Engineering' => [
+                'D3 Electronics Engineering',
+                'D3 Telecommunication Engineering',
+                'D3 Electrical Engineering',
+                'D4 Digital Telecommunication Networks',
+                'D4 Power Systems',
+                'D4 Electronics Engineering'
+            ],
+            'Civil Engineering' => [
+                'D3 Civil Engineering',
+                'D3 Mining Engineering',
+                'D3 Road, Bridge, and Water Construction Engineering Technology',
+                'D4 Construction Engineering Management',
+                'D5 Road and Bridge Construction Engineering Technology'
+            ],
+        ];
+
+        foreach ($departments as $department => $studyPrograms) {
+            $department = Department::create([
+                'name' => $department
+            ]);
+
+            foreach ($studyPrograms as $studyProgram) {
+                StudyProgram::create([
+                    'name' => $studyProgram,
+                    'department_id' => $department->id
+                ]);
+            }
+        }
     }
 
     /**
