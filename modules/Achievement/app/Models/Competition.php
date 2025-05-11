533dd7500cceb724f5386f27e30a94804bd8572e
KhenCahyo13 <khencahyo02@gmail.com>
2025-05-11 12:57:16 +0700

    refactor: Model media

--- modules/Achievement/app/Models/Competition.php
+++ modules/Achievement/app/Models/Competition.php
@@ -2,11 +2,11 @@
 
 namespace Modules\Achievement\Models;
 
-use App\Models\User;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Modules\Master\Models\User;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 use Tonysm\RichTextLaravel\Models\Traits\HasRichText;
