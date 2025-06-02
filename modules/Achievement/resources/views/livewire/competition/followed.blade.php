75bdb046d30fa5c11a8853b0eb049bf9faa6d448
KhenCahyo13 <khencahyo02@gmail.com>
2025-06-03 06:55:29 +0700

    refactor: Undefined variable

--- modules/Achievement/resources/views/livewire/competition/followed.blade.php
+++ modules/Achievement/resources/views/livewire/competition/followed.blade.php
@@ -32,13 +32,10 @@
     {{-- Data List --}}
     <div class="grid grid-cols-1 gap-5 md:grid-cols-2">
         @forelse ($competitions as $competition)
-            @php
-                $posterPath = $competition->competition->getFirstMediaUrl('poster');
-            @endphp
             <div
                 class="flex flex-col gap-5 rounded-xl border border-gray-200 bg-white p-4 dark:border-gray-800 dark:bg-white/[0.03] sm:flex-row sm:items-center sm:gap-6">
                 <div class="w-full h-32 overflow-hidden rounded-lg md:w-72 md:h-40">
-                    <img src="{{ asset("$posterPath") }}" alt="Poster" class="overflow-hidden rounded-lg w-full h-full object-cover">
+                    <img src="{{ asset($competition->competition->getFirstMediaUrl('poster')) }}" alt="Poster" class="overflow-hidden rounded-lg w-full h-full object-cover">
                 </div>
 
                 <div>
