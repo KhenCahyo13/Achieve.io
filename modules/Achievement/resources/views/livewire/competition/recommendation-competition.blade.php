604c7411237b1326c83d1f11ca443bdb117b3417
KhenCahyo13 <khencahyo02@gmail.com>
2025-06-05 04:45:46 +0700

    refactor: Undefined variable

--- modules/Achievement/resources/views/livewire/competition/recommendation-competition.blade.php
+++ modules/Achievement/resources/views/livewire/competition/recommendation-competition.blade.php
@@ -154,7 +154,7 @@
                     <div
                         class="flex flex-col gap-5 rounded-xl border border-gray-200 bg-white p-4 dark:border-gray-800 dark:bg-white/[0.03] sm:flex-row sm:items-center sm:gap-6">
                         <div class="w-full h-32 overflow-hidden rounded-lg md:w-72 md:h-40">
-                            <img src="{{ asset("$posterPath") }}" alt="Poster"
+                            <img src="{{ asset($competition->competition->getFirstMediaUrl('poster')) }}" alt="Poster"
                                 class="overflow-hidden rounded-lg w-full h-full object-cover">
                         </div>
 
