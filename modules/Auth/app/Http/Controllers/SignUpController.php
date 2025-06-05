d2a7bc5735427ab694e25b0b7036bf3fb6d0d1d2
KhenCahyo13 <khencahyo02@gmail.com>
2025-06-05 12:19:19 +0700

    refactor: Redirect on SignIn & SignUp

--- modules/Auth/app/Http/Controllers/SignUpController.php
+++ modules/Auth/app/Http/Controllers/SignUpController.php
@@ -3,6 +3,7 @@
 namespace Modules\Auth\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class SignUpController extends Controller
 {
@@ -11,6 +12,10 @@ class SignUpController extends Controller
      */
     public function index()
     {
+        if (Auth::check()) {
+            return redirect()->route('core.dashboard');
+        }
+
         return view('auth::pages.signup');
     }
 }
